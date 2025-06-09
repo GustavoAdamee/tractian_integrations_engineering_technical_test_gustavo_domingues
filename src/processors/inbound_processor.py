186c7a05a3f9a7a5e1a7d751e1b3a4a03582ce59
Adame <gusadomingues@gmail.com>
2025-06-09 19:42:42 -0300

    feat - inboud processor add

---
+++ src/processors/inbound_processor.py
@@ -0,0 +1,24 @@
+from src.core.tracos_handler import TracOsHandler
+from src.core.customer_handler import CustomerHandler
+from src.core.translator import Translator
+from setup import TracOSWorkorder
+from setup import CustomerSystemWorkorder
+
+class InboundProcessor:
+    def __init__(self):
+        self.tracos_handler = TracOsHandler()
+        self.customer_handler = CustomerHandler()
+        self.translator = Translator()
+
+    async def process(self) -> None:
+        await self.tracos_handler.connect()
+        
+        inbound_workorder: list[CustomerSystemWorkorder] = []
+        inbound_workorder = self.customer_handler.get_workorders()
+        
+        translated_workorders: list[TracOSWorkorder] = []
+        for workerorder in inbound_workorder:
+            translated_workorders.append(self.translator.customer_to_tracos(workerorder))
+
+        for workorder in translated_workorders:
+            await self.tracos_handler.create_workorder(workorder)
